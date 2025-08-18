2bc0fc1082129d8d89daa536fc43dd35a8022808
shujanshaikh <shaikhshujan@gmail.com>
2025-08-18 20:44:39 +0530

    fix

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,6 +1,2 @@
 import { createAuthClient } from "better-auth/react"
-import { env } from "~/env"
-export const authClient = createAuthClient({
-
-    baseURL : env.BETTER_AUTH_URL || "http://localhost:3000"
-})
+export const authClient = createAuthClient()
