8dd1f2c5d12d4a08772e851239c3785fe2586ebb
shujanshaikh <shaikhshujan@gmail.com>
2025-08-29 14:21:45 +0530

    small issue fix

--- src/lib/polar.ts
+++ src/lib/polar.ts
@@ -5,5 +5,5 @@ export const polarClient = new Polar({
     // Use 'sandbox' if you're using the Polar Sandbox environment
     // Remember that access tokens, products, etc. are completely separated between environments.
     // Access tokens obtained in Production are for instance not usable in the Sandbox environment.
-    server: "sandbox"
+    server: "production"
 });
